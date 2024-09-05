c53dafbc58ab2ae837636b9fd58295f00eb5b1e3
mezzopera <168661847+mezzopera@users.noreply.github.com>
2024-09-05 19:17:39 +0200

    Update omen_hub.lua

--- omen_hub.lua
+++ omen_hub.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/mezzopera/Omen-Hub/main/OmenMain.lua'))()
--- Updated Loadstring
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66e9f26a9c5bae17e43b49671129450d.lua"))()
